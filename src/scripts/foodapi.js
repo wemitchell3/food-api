dc94ff2ef6cb1656ab049318dc54e0588b21a66e
William Mitchell <wemitchell3@gmail.com>
2019-03-21 20:35:00 -0500

    Uploading finished displaying foods exercise.

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,14 +1,17 @@
-//Create a function which returns a string template. The template is the HTML representation for a food item.
-const foodList = document.querySelector(".foodList")
+const apiUrl = "http://localhost:8088"
 
-
-fetch("http://localhost:8088/food")
+const getAllFoods = () => fetch(`${apiUrl}/food`)
     .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-            const foodAsHTML = foodFactory(food)
-            addFoodToDom(foodAsHTML)
-        })
-    })
+
+
+
+
+    // .then(parsedFoods => {
+    //     console.table(parsedFoods)
+    //     parsedFoods.forEach(food => {
+    //         const foodAsHTML = foodFactory(food)
+    //         addFoodToDom(foodAsHTML)
+    //     })
+    // })
 
 
